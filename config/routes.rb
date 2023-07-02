e2f008a9545f61a9bc2e045bca6332523862bae8
Michal <khakaimic4@gmail.com>
2023-07-02 12:35:12 +0300

    completed lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :recipes
+  resources :users
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  post '/signup', to: 'users#create'
+  delete "/logout", to: "session#destroy"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
